75a76a7f6f059d74a6c51567b5a3c9de005c09ff
FranciscoME <francisco.elguezabal@gmail.com>
2015-07-21 13:10:03 -0500

    ACF function

--- DataProcessor.R
+++ DataProcessor.R
@@ -42,3 +42,29 @@ suppressMessages(library (xts))        # Time series utilities
 if (!require(zoo)) install.packages('zoo', quiet = TRUE)
 suppressMessages(library (zoo))        # Time series utilities
 
+# -- AutoCorrelation and Partial AutoCorrelation Calculations and Graph ------------------------- #
+
+AutoCorrelation <- function(x, type, YAxisText)
+{
+  ejeX   <- "Lags"
+  ciline <- 2/sqrt(length(x))
+  bacf   <- acf(x, plot = FALSE, lag.max = 22, type = type)
+  bacfdf <- with(bacf, data.frame(lag, acf))
+  bacfdf <- bacfdf[-seq(1, 0),]
+  Sig_nc <- (abs(bacfdf[,2]) > abs(ciline))^2
+  bacfdf <- cbind(bacfdf, Sig_nc)
+  
+  facgg <- qplot(lag,acf,data=bacfdf,geom="bar",width=.75, stat = "identity",
+  position = "identity", ylab = "Statistical Significance", xlab = ejeX,
+  fill = factor(Sig_nc)) + labs(title = YAxisText)                                     +  
+  geom_hline(yintercept=-ciline, color="dark grey", size=0.35, linetype = "dashed")                                                     +
+  geom_hline(yintercept= ciline, color="dark grey", size=0.35, linetype = "dashed")                                                     +
+  geom_hline(yintercept = 0, color = "dark grey",size = 0.25)                          +
+  scale_fill_hue(breaks = 0:1,labels=c("Not Significative",
+  "Significative"),h=c(260, 235)) + scale_x_continuous(breaks = seq(1,22,1))           +
+  theme(panel.background = element_rect(size = .75,colour = "dark grey",fill="white"), 
+  axis.title.x = element_text(size = 10), axis.title.y = element_text(size = 10),
+  plot.title = element_text(size = 14,vjust = 1), legend.position = "bottom",
+  legend.title = element_blank(),legend.text = element_text(colour="blue",size = 10))
+  return(facgg)
+}
